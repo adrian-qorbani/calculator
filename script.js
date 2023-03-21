af1378cb6ac7f38a91911ac32d67e2b6feff8eb9
Adrian <mahdigh@live.com>
2023-03-21 21:26:24 +0330

    linked dom elements with associated vars within script

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// Defined Calculating Functions
+///// Defined Calculating Functions /////
 
 const add = (x, y) => x + y;
 const subtract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
 const divide = (x, y) => a / b;
 
-// Defined vars for Operation
+///// Defined vars for Operation /////
 
 const firstNum = 4; //MUST BE DYNAMIC
 const secondNum = 6; //MUST BE DYNAMIC
 const operators = ["add", "subtract", "multiply", "divide"];
 let selectOperator = "add"; //MUST BE DYNAMIC
 
-// Defined Calc Operation
+///// Defined Calc Operation /////
 
 const execCalculation = (operator, [first, second]) => {
   switch (operator) {
@@ -33,5 +33,31 @@ const execCalculation = (operator, [first, second]) => {
   }
 };
 
-// DOM Variables
+///// DOM Variables /////
+// Operators
+const btnAdd = document.querySelector("#add-operator");
+const btnSubtract = document.querySelector("#subtract-operator");
+const btnDivide = document.querySelector("#division-operator");
+const btnMultiply = document.querySelector("#times-operator");
 
+// Numbers
+const btn1 = document.querySelector("#num-1");
+const btn2 = document.querySelector("#num-2");
+const btn3 = document.querySelector("#num-3");
+const btn4 = document.querySelector("#num-4");
+const btn5 = document.querySelector("#num-5");
+const btn6 = document.querySelector("#num-6");
+const btn7 = document.querySelector("#num-7");
+const btn8 = document.querySelector("#num-8");
+const btn9 = document.querySelector("#num-9");
+const btn10 = document.querySelector("#num-0");
+
+// Others
+const btnDecimal = document.querySelector("#btn-decimal");
+const btnAC = document.querySelector("#btn-ac");
+const btnEqual = document.querySelector("#btn-equal");
+
+// TESTING ZONE!!!!!!
+// const adding = btnAdd.addEventListener("click", () => {
+//   console.log("YAAAY!!!ADDING!");
+// });
