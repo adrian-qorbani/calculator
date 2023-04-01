e2ed2b2db96668fa56f5c20b8b9231b12e87bbbd
Adrian <mahdigh@live.com>
2023-04-01 09:54:01 +0330

    cleaned the code. Reduced func sizes.

--- script.js
+++ script.js
@@ -10,9 +10,8 @@ const divide = (x, y) => a / b;
 let firstNum = [];
 let secondNum = [];
 const operators = ["add", "subtract", "multiply", "divide"];
-let selectOperator = "add"; //MUST BE DYNAMIC
-let displayValue;
 let currentValue = [];
+let selectOperator;
 
 ///// DOM Variables /////
 // Operators
@@ -20,6 +19,7 @@ const btnAdd = document.querySelector("#add-operator");
 const btnSubtract = document.querySelector("#subtract-operator");
 const btnDivide = document.querySelector("#division-operator");
 const btnMultiply = document.querySelector("#times-operator");
+let calculatorDisplayInput = document.getElementById("calculator-input");
 
 // Others
 const btnDecimal = document.querySelector("#btn-decimal");
@@ -31,13 +31,11 @@ const btnEqual = document.querySelector("#btn-equal");
 const execCalculation = (operator, [first, second]) => {
   switch (operator) {
     case "add":
+
       // Using Number() to turn stringifed charecters to numbers for calculation
       console.log(add(Number(first), Number(second)));
-      document.getElementById("calculator-input").value = add(
-        Number(first),
-        Number(second)
-      );
-      
+      calculatorDisplayInput.value = add(Number(first), Number(second));
+
       break;
     case "subtract":
       console.log(subtract(Number(first), Number(second)));
@@ -67,68 +65,12 @@ const execCalculation = (operator, [first, second]) => {
   }
 };
 
-// TESTING ZONE!!!!!!
-// const adding = btnAdd.addEventListener("click", () => {
-//   console.log("YAAAY!!!ADDING!");
-// });
+
+// Operator Assignment
+btnAdd.addEventListener("click", () => runOperation(0));
+btnSubtract.addEventListener("click", () => runOperation(1));
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// Assigning
-btnAdd.addEventListener("click", () => runAdd());
-
-
-let runAdd = () => {
-  if (firstNum.length == 0) {
-    firstNum = currentValue;
-    currentValue = [];
-    selectOperator = operators[0];
-    execCalculation(selectOperator, [
-      firstNum.join(""),
-      secondNum.join(""),
-    ]);
-
-
-  } else {
-    secondNum = currentValue;
-    selectOperator = operators[0];
-    execCalculation(selectOperator, [
-      firstNum.join(""),
-      secondNum.join(""),
-    ]);
-    secondNum = [];
-    firstNum = document.getElementById("calculator-input").value.split('');
-    currentValue = [];
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-// deactivated temporarily for maintenance
-// btnSubtract.addEventListener("click", () => (selectOperator = operators[1]));
-// btnDivide.addEventListener("click", () => (selectOperator = operators[2]));
-// btnMultiply.addEventListener("click", () => (selectOperator = operators[3]));
 
 const actionButtons = document.querySelectorAll(".action-btns");
 
@@ -137,7 +79,6 @@ actionButtons.forEach((actionBtn) =>
   actionBtn.addEventListener("click", () => {
     currentValue.push(actionBtn.value);
     document.getElementById("calculator-input").value = currentValue.join("");
-    displayValue = currentValue.join("");
   })
 );
 // OPERATION: AC Function
@@ -152,3 +93,20 @@ btnEqual.addEventListener("click", () => {
     secondNum.join(""),
   ]);
 });
+
+
+// OPERATION: General
+let runOperation = (op) => {
+  selectOperator = operators[op];
+  if (firstNum.length == 0) {
+    firstNum = currentValue;
+    currentValue = [];
+    execCalculation(selectOperator, [firstNum.join(""), secondNum.join("")]);
+  } else {
+    secondNum = currentValue;
+    execCalculation(selectOperator, [firstNum.join(""), secondNum.join("")]);
+    // secondNum = [];
+    firstNum = calculatorDisplayInput.value.split("");
+    currentValue = [];
+  }
+};
