822712623ab2acdf51db87116cc72a3b67dc3110
Adrian <mahdigh@live.com>
2023-04-08 00:53:54 +0330

    removed wip status from html

--- script.js
+++ script.js
@@ -5,104 +5,111 @@ const subtract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
 
-///// Defined vars for Operation /////
-
-
+///// Defined a object calculation vars for Operation /////
 const calculatorObj = {
   firstNum: [],
   secondNum: [],
-  operators: ["add", "subtract", "multiply", "divide"],
+  operators: ["+", "-", "*", "/"],
   currentValue: [],
   currentOperator: null,
 };
-///// DOM Variables /////
-// Operators
-const btnAdd = document.querySelector("#add-operator");
-const btnSubtract = document.querySelector("#subtract-operator");
-const btnDivide = document.querySelector("#division-operator");
-const btnMultiply = document.querySelector("#times-operator");
-const actionButtons = document.querySelectorAll(".action-btns");
-let calculatorDisplayInput = document.querySelector("#calculator-input");
 
-// Others
-const btnDecimal = document.querySelector("#btn-decimal");
-const btnAC = document.querySelector("#btn-ac");
-const btnEqual = document.querySelector("#btn-equal");
+///// DOM Variables /////
+const calculatorDisplayInput = document.querySelector("#calculator-input");
+// Added event listeners for every button within #calc-buttons
+const calcBtn = document.querySelector("#calc-buttons");
+calcBtn.addEventListener("click", (event) => {
+  const { target } = event;
+  // If not a button, return and doesnt do anything.
+  if (!target.matches("button")) {
+    return;
+  }
+  // If a button, actions happen depending on button's duty
+  if (target.classList.contains("operator")) {
+    switch (target.value) {
+      case "+":
+        calculatorObj.currentOperator = 0;
+        runOperation(0);
+        break;
+      case "-":
+        calculatorObj.currentOperator = 1;
+        runOperation(1);
+        break;
+      case "*":
+        calculatorObj.currentOperator = 2;
+        runOperation(2);
+        break;
+      case "/":
+        calculatorObj.currentOperator = 3;
+        runOperation(3);
+        break;
+    }
+    return;
+  }
+
+  if (target.classList.contains("decimal")) {
+    if (!calculatorDisplayInput.value.includes(".")) {
+      updateCalcDisplay(".");
+      return;
+    } else {
+      return;
+    }
+  }
+
+  if (target.classList.contains("all-clear")) {
+    calculatorObj.firstNum = [];
+    calculatorObj.currentValue = [];
+    calculatorObj.secondNum = [];
+    calculatorDisplayInput.value = 0;
+    return;
+  }
+
+  if (target.classList.contains("equal-sign")) {
+    runOperation(calculatorObj.currentOperator);
+    return;
+  }
+  updateCalcDisplay(target.value);
+});
 
 ///// Defined Calc Operation /////
 
 const execCalculation = (operator, [first, second]) => {
   switch (operator) {
-    case "add":
+    case "+":
       // Using Number() to turn stringifed charecters to numbers for calculation
       calculatorDisplayInput.value = add(Number(first), Number(second));
       break;
-    case "subtract":
+    case "-":
       calculatorDisplayInput.value = subtract(Number(first), Number(second));
       break;
-    case "multiply":
+    case "*":
       calculatorDisplayInput.value = multiply(Number(first), Number(second));
       break;
-    case "divide":
+    case "/":
       calculatorDisplayInput.value = divide(Number(first), Number(second));
       break;
   }
 };
 
-// Buttons Choosing Operators
-btnAdd.addEventListener("click", () => {
-  calculatorObj.currentOperator = 0;
-  runOperation(0);
-});
-
-btnSubtract.addEventListener("click", () => {
-  calculatorObj.currentOperator = 1;
-  runOperation(1);
-});
-
-btnMultiply.addEventListener("click", () => {
-  calculatorObj.currentOperator = 2;
-  runOperation(2);
-});
-btnDivide.addEventListener("click", () => {
-  calculatorObj.currentOperator = 3;
-  runOperation(3);
-});
-
-// Operation : Entering Numbers in Display
-actionButtons.forEach((actionBtn) =>
-  actionBtn.addEventListener("click", () => {
-    calculatorObj.currentValue.push(actionBtn.value);
-    calculatorDisplayInput.value = calculatorObj.currentValue.join("");
-  })
-);
-
-// OPERATION: AC Function
-btnAC.addEventListener("click", () => {
-  calculatorObj.firstNum = [];
-  calculatorObj.currentValue = [];
-  calculatorObj.secondNum = [];
-  calculatorDisplayInput.value = 0;
-});
-
-// OPERATION: Calculate
-btnEqual.addEventListener("click", () =>
-  runOperation(calculatorObj.currentOperator)
-);
-
-// OPERATION: General Calculation
+//// Defined functions
 let runOperation = (op) => {
   let selectOperator = calculatorObj.operators[op];
+  // If there's no first input number
   if (calculatorObj.firstNum.length == 0) {
     calculatorObj.firstNum = calculatorObj.currentValue;
-    calculatorObj.currentValue = [];
+    emptyCurrentValue();
     execCalculation(selectOperator, [
       calculatorObj.firstNum.join(""),
       calculatorObj.secondNum.join(""),
     ]);
-  } else {
+  }
+  // else there's already a first input number
+  else {
     calculatorObj.secondNum = calculatorObj.currentValue;
-    // If secondNum is 0, running execution for division and multiplication will return zero, therefore following code prevents it.
+
+    // If secondNum is 0, running execution for division and
+    // multiplication will return zero, therefore following code prevents it.
+
     if (
       (selectOperator == calculatorObj.operators[2] &&
         calculatorObj.secondNum == 0) ||
@@ -116,6 +123,14 @@ let runOperation = (op) => {
       calculatorObj.secondNum.join(""),
     ]);
     calculatorObj.firstNum = calculatorDisplayInput.value.split("");
-    calculatorObj.currentValue = [];
+    emptyCurrentValue();
   }
 };
+
+const updateCalcDisplay = (prop) => {
+  calculatorObj.currentValue.push(prop);
+  calculatorDisplayInput.value = calculatorObj.currentValue.join("");
+};
+
+const emptyCurrentValue = () => calculatorObj.currentValue = [];
+
